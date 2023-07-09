e4bee4820c48e3ce3245ac08c1757967c9bfb58a
yue <yue@YUE-NB>
2023-07-09 16:33:48 +0800

    加入專案檔案。

---
+++ cocTest/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cocTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() 
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Introduction()
+        {
+            
+
+            return View();
+        }
+        
+        public ActionResult HomeworkD1()
+        {
+            
+
+            return View();
+        }
+        
+        public ActionResult coc()
+        {
+            return View();
+        }
+    }
+}
